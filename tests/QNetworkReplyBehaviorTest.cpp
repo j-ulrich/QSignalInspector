238e5e8665c9dd50059f79f9341363eaa3f7f8c2
Jochen Ulrich <jochenulrich@t-online.de>
2017-11-26 22:44:57 +0100

    Improves QNetworkReplyBehaviorTest

--- tests/QNetworkReplyBehaviorTest.cpp
+++ tests/QNetworkReplyBehaviorTest.cpp
@@ -5,19 +5,29 @@
 
 #include "QSignalInspector.h"
 
-#include <QTextStream>
-#include <QNetworkAccessManager>
-#include <QNetworkRequest>
-#include <QNetworkReply>
-#include <QUrl>
 #include <QCoreApplication>
+#include <QFile>
+#include <QNetworkAccessManager>
+#include <QNetworkReply>
+#include <QNetworkRequest>
 #include <QSharedPointer>
+#include <QTextStream>
+#include <QUrl>
 
 #include <cstdio>
+#include <iostream>
 
-QSharedPointer<QSignalInspector> executeRequest(const QNetworkRequest& req, QNetworkAccessManager& qNam)
+QSharedPointer<QSignalInspector> executeRequest(QNetworkAccessManager::Operation op,
+                                                const QNetworkRequest& req,
+                                                QNetworkAccessManager& qNam,
+                                                const QByteArray& data = QByteArray())
 {
-	QSharedPointer<QNetworkReply> reply{qNam.get(req)};
+	QSharedPointer<QNetworkReply> reply;
+	switch (op)
+	{
+	case QNetworkAccessManager::GetOperation: reply.reset(qNam.get(req)); break;
+	case QNetworkAccessManager::PostOperation: reply.reset(qNam.post(req, data)); break;
+	}
 	auto inspector = QSharedPointer<QSignalInspector>::create(reply.data());
 
 	QEventLoop eventLoop;
@@ -33,21 +43,41 @@ void printSignals(QTextStream& out, QSharedPointer<QSignalInspector> inspector)
 	{
 		QStringList parameterValues;
 		for (auto parameter : signalEmission.second)
-			parameterValues << parameter.toString();
+		{
+			if (parameter.canConvert<QString>())
+				parameterValues << parameter.toString();
+			else
+				parameterValues << QString::fromUtf8(parameter.typeName());
+		}
 		out << signalEmission.first.name() << "(" << parameterValues.join(", ") << ")" << endl;
 	}
 }
 
-void executeAndPrint(const QString& url, QTextStream& out, QNetworkAccessManager& qNam)
+void executeAndPrint(QNetworkAccessManager::Operation op,
+                  const QString& url,
+                  QTextStream& out,
+                  QNetworkAccessManager& qNam,
+                  const QByteArray& body = QByteArray(),
+                  const QByteArray& contentType = QByteArray())
 {
-	out << url << endl;
-	out << QString{url.size(), '-'} << endl;
+	QString verb;
+	switch (op)
+	{
+	case QNetworkAccessManager::GetOperation: verb = "GET"; break;
+	case QNetworkAccessManager::PostOperation: verb = "POST"; break;
+	}
+
+	out << verb << " " << url << endl;
+	out << QString{verb.size() + 1 + url.size(), '-'} << endl;
 
 	QNetworkRequest req{QUrl{url}};
-	printSignals(out, executeRequest(req, qNam));
+	if (!contentType.isEmpty())
+		req.setHeader(QNetworkRequest::ContentTypeHeader, contentType);
+	printSignals(out, executeRequest(op, req, qNam, body));
 
 	out << endl;
 }
+
 
 int main(int argc, char **argv)
 {
@@ -55,7 +85,11 @@ int main(int argc, char **argv)
 	QTextStream out{stdout};
 	QNetworkAccessManager qNam;
 
-	executeAndPrint("http://eu.httpbin.org/status/200", out, qNam);
-	executeAndPrint("http://eu.httpbin.org/status/500", out, qNam);
-	executeAndPrint("http://eu.httpbin.org/get", out, qNam);
+	executeAndPrint(QNetworkAccessManager::GetOperation, "http://eu.httpbin.org/status/200", out, qNam);
+	executeAndPrint(QNetworkAccessManager::GetOperation, "http://eu.httpbin.org/status/500", out, qNam);
+	executeAndPrint(QNetworkAccessManager::GetOperation, "http://eu.httpbin.org/get", out, qNam);
+	executeAndPrint(QNetworkAccessManager::PostOperation, "http://eu.httpbin.org/post", out, qNam,
+	                "{ \"random\": \"gLINDuERfaRBQesd6FuF2DJ0vMaVvHDlCPm9ib2DnIIrgSwVSYQdlb5RvUiK89EZIJMeCQOk1iRRDYSiCN8sdpvehD9JP1eKWxfTzZtLLUlAEVVKBqmz4oheyJYcLpWxNnxPlAUHmJ3I09ZYli99sF9wM1Amhjnj4vKwWuWSJRtsQZrJdMDdWteUjnxQQy4SyTzVoXlFxljLu96WAc7kOPREd9brDzyfVGxOJ79cnwO8M3wBLExAwVX4fdLf9qbPz0UsErefH9t1Sy2hVeGFKzeewRLN97djADR\" }",
+	                "application/json");
+
 }
